1f50c539e59d4c4f46b1c006ad601f5ed7666685
Ariele Bonifacio <arielebonifacio@MBP-de-Ariele.fritz.box>
2022-09-19 12:57:28 +0200

    layout change

---
+++ js/index.js
@@ -0,0 +1,46 @@
+function showPassword() {
+    const eye = document.getElementById ('eye');
+    const eyeSlash = document.getElementById ('eye-slash');
+    const fieldPassword = document.getElementById ('field-password');
+
+    if (eye.style.display === 'none') {
+        eye.style.display = 'block';
+        eyeSlash.style.display = 'none';
+        fieldPassword.type = 'text';
+    }    else {
+        eye.style.display = 'none';
+        eyeSlash.style.display = 'block';
+        fieldPassword.type = 'password';
+    }
+};
+
+function handleCredentialResponse(response) {
+    const data = jwt_decode(response.credential)
+    console.log(data)
+
+    fullName.textContent = data.name
+    email.textContent = data.email
+    picture.setAttribute("src", data.picture)
+
+  }
+  window.onload = function () {
+
+    google.accounts.id.initialize({
+      client_id: "421333481114-iptv0fm5p8kcns3m4ug2lg1ubo5dlvs0.apps.googleusercontent.com",
+      callback: handleCredentialResponse
+    }); // initialize login configuration
+
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"), {
+            theme: "filled_blue",
+            size: "large",
+            type: "standard",
+            shape: "pill",
+            text: "$ {button.text}",
+            size: "large",
+            logo_alignment: "left"
+        }
+        );
+
+    google.accounts.id.prompt(); // also display the One Tap dialog
+  }
